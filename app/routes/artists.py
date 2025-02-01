0e6f645c6ce4c246efd8f964e0753a8621a959b5
Nir Levy <nir.l@taboola.com>
2025-02-01 10:57:54 +0200

    changing number of artists

--- app/routes/artists.py
+++ app/routes/artists.py
@@ -11,6 +11,6 @@ def bands():
         return redirect(url_for('login', next=request.url))
         
     spotify_api = spotipy.Spotify(auth=session['token_info']['access_token'])
-    resp = spotify_api.current_user_top_artists(time_range='medium_term', limit=20)
+    resp = spotify_api.current_user_top_artists(time_range='medium_term', limit=21)
     bands = resp['items']
     return render_template('bands.html', bands=bands) 
